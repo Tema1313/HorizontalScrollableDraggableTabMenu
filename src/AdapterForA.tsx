b63acc5644dece0dc3ca3bafc858bf64f9d29b2f
Tema1313 <88036915+Tema1313@users.noreply.github.com>
2023-06-13 16:57:16 +0700

    Теперь на typesript

--- src/AdapterForA.tsx
+++ src/AdapterForA.tsx
@@ -1,5 +1,5 @@
 
-import React, { useState, useEffect, TouchEvent, useRef } from 'react';
+import React, { useState, useEffect, TouchEvent, useRef, MutableRefObject } from 'react';
 import axios from 'axios';
 import "./style.css"
 import * as RB from "react-bootstrap"
@@ -40,14 +40,15 @@ function App() {
       city: "Amsterdam"
     }
   ]
-  const tabMenu = useRef();
+  const tabMenu = useRef(null);
   const [activeDrag, setActiveDrag] = useState(false)
-  const [ active, setActive ] = useState(0);
+  const [active, setActive] = useState(0);
 
-  const handleMouseMove = (drag) => {
+  const handleMouseMove = (drag: any) => {
     if (!activeDrag) return;
     tabMenu.current.scrollLeft -= drag.movementX;
     tabMenu.current.classList.add("dragging")
+
   }
 
   document.addEventListener("mouseup", () => {
@@ -59,7 +60,7 @@ function App() {
     setActiveDrag(true)
   }
 
-  const openTab = e => setActive(+e.target.dataset.index);
+  const openTab = (e: React.MouseEvent<HTMLElement>) => setActive(+e.currentTarget.dataset.index);
 
   return (
     <section className="main-container">
@@ -67,14 +68,14 @@ function App() {
         <div className="tab-navigation">
           <ul ref={tabMenu} onMouseMove={(drag) => handleMouseMove(drag)} onMouseDown={() => handleMouseDown()} className="tab-menu" >
             {array.map((city, key) => (
-              <li data-index={key} onClick={openTab} key={key} className={`${key === active ? "active tab-btn" : "tab-btn"}`}>{city.city}</li>
+              <li data-index={key} onClick={(e: React.MouseEvent<HTMLElement>) => openTab(e)} key={key} className={`${key === active ? "active tab-btn" : "tab-btn"}`}>{city.city}</li>
             ))}
           </ul>
         </div>
       </div>
       <div className='tab-content'>
         {array.map((city, key) => (
-          <div key={key} className={`${key===active ? "tab active" : "tab"}`}>
+          <div key={key} className={`${key === active ? "tab active" : "tab"}`}>
             <div className='row'>
               {city.city}
             </div>
